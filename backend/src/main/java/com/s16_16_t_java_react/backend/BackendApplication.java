8faaf5c57a8116c515ac05817496fc2a68581e38
chuttmateo <chuttmateo0@gmail.com>
2024-07-08 16:43:35 -0300

    Add backend project

---
+++ backend/src/main/java/com/s16_16_t_java_react/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.s16_16_t_java_react.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/s16_16_t_java_react/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s16_16_t_java_react.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
