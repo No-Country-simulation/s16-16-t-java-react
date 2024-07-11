74ac3193efed14c7eafd70b18b80fc0d83d37c06
arthurcp96 <arthurcp96@gmail.com>
2024-07-11 17:55:38 -0300

    Usuario, categorias entities.

---
+++ backend/src/main/java/com/s16_16_t_java_react/backend/entities/Categorias.java
@@ -0,0 +1,34 @@
+package com.s16_16_t_java_react.backend.entities;
+
+
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Categorias")
+public class Categorias {
+    @Id
+    @Column(name="idusuario", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;    
+    @Column(nullable = false)
+    @Nonnull
+    private String nombre;
+    
+}
