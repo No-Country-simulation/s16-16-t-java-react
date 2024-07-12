7a0e46e29a7888507bd811defb0de7953fb27848
Franco <francofleitasguti@gmail.com>
2024-07-12 16:59:28 -0300

    creacion carpetas y paleta tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-light': '#e8f7fc',
+        'primary-light-hover': '#dcf3fa',
+        'primary-light-active': '#b7e6f5',
+        'primary-normal': '#16afe0',
+        'primary-normal-hover': '#149eca',
+        'primary-normal-active': '#128cb3',
+        'primary-dark': '#1183a8',
+        'primary-dark-hover': '#1183a8',
+        'primary-dark-active': '#0a4f65',
+        'primary-darker': '#0a4f65',
+        'neutral-light': '#ffffff',
+        'neutral-light-hover': '#fefefe',
+        'neutral-light-active': '#fdfdfd',
+        'neutral-normal': '#fafafa',
+        'neutral-normal-hover': '#e1e1e1',
+        'neutral-normal-active': '#c8c8c8',
+        'neutral-dark': '#bcbcbc',
+        'neutral-dark-hover': '#969696',
+        'neutral-dark-active': '#707070',
+        'neutral-darker': '#393939'
+      }
+    },
   },
   plugins: [],
 };
