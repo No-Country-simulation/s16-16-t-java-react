60c2ad439dca1cf753198d74ed2b7276f230b18c
juanRCoder <guillecarck@gmail.com>
2024-07-12 15:06:56 -0500

    feat: add folder components and pages

---
+++ frontend/src/components/componente1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function componente1() {
+  return (
+    <div>componente1</div>
+  )
+}
+
+export default componente1
---
+++ frontend/src/pages/page1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page1() {
+  return (
+    <div>page1</div>
+  )
+}
+
+export default page1
