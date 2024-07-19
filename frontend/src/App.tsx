4682d966cc7d43e06a4a3c4c9ae329908ec8ed7b
OmarGaviria <omargaviria9@gmail.com>
2024-07-19 15:11:14 -0500

    feat  agregando paginas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
+import BannerPasos from './components/BannerPasos';
+import Calculadora from './components/Calculadora';
 
+
+import Materiales from './components/Materiales';
+
+import Tabla3d from './components/Tabla3d';
 function App() {
 
 
   return (
     <>
       <h1>Hola devs!</h1>
+      <BannerPasos/>
+      <Calculadora/>
+      <Materiales/>
+      <Tabla3d/>
+    
     </>
   )
 }
