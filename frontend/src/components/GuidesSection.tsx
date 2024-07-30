9976c8b308c778bff08b76331cc52eb656bad000
Franco <francofleitasguti@gmail.com>
2024-07-30 17:12:36 -0300

    Fix Home

--- frontend/src/components/GuidesSection.tsx
+++ frontend/src/components/GuidesSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'wouter';
 
 const GuidesSection = () => {
   return (
@@ -8,12 +9,12 @@ const GuidesSection = () => {
       <div className='flex flex-col gap-6'>
         <h3 className='text-start text-xl font-semibold leading-6'>Aprende Todo lo que Necesites</h3>
         <p className='text-lg font-normal leading-[1.35rem] text-left'>Explora nuestras detalladas guías para principiantes y expertos. Aprende las mejores prácticas, técnicas avanzadas y consejos útiles para mejorar tus habilidades de diseño 3D.</p>
-        <p className='cursor-pointer text-start text-lg font-normal leading-[1.35rem] underline text-primary-normal w-fit hover:text-neutral-normal'>Ver Guías</p>
+        <Link to='/designGuide' className='cursor-pointer text-start text-lg font-normal leading-[1.35rem] underline text-primary-normal w-fit hover:text-neutral-normal'>Ver Guías</Link>
       </div>
       <div className='flex flex-col gap-6'>
         <h3 className='text-xl font-semibold leading-6 text-start'>Guías de Materiales</h3>
-        <p className='text-lg font-normal leading-[1.35rem] text-start'>Explora nuestras detalladas guías para principiantes y expertos. Aprende las mejores prácticas, técnicas avanzadas y consejos útiles para mejorar tus habilidades de diseño 3D.</p>
-        <p className='cursor-pointer w-fit text-start text-lg font-normal leading-[1.35rem] underline text-primary-normal hover:text-neutral-normal'>Ver Guías</p>
+        <p className='text-lg font-normal leading-[1.35rem] text-start'>Descubre las características, ventajas y aplicaciones de los diferentes materiales utilizados en la impresión 3D. Nuestra tabla comparativa te ayudará a elegir el material perfecto para tus proyectos.</p>
+        <Link to='/calculator' className='cursor-pointer w-fit text-start text-lg font-normal leading-[1.35rem] underline text-primary-normal hover:text-neutral-normal'>Explorar las tablas</Link>
       </div>
       </div>
     </section>
